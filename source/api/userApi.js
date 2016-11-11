23c3f346fe7d791ddbe123109f3acfa73fef5c36
SamGraber <authorsamgraber@gmail.com>
2016-11-11 14:30:54 -0500

    Hit the mock api when running on localhost

--- source/api/userApi.js
+++ source/api/userApi.js
@@ -1,9 +1,13 @@
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
+
 export function getUsers() {
 	return get('users');
 }
 
 function get(url) {
-	return fetch(url).then(onSuccess, onError);
+	return fetch(baseUrl + url).then(onSuccess, onError);
 }
 
 function onSuccess(response) {

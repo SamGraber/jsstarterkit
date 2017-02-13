b4dacecee308d8e6e112201dccc5e904e12e2bf3
SamGraber <authorsamgraber@gmail.com>
2017-02-13 10:14:59 -0500

    Changed the production base url to heroku

--- source/api/baseUrl.js
+++ source/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-	return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+	return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://floating-tor-95497.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
